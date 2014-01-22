d65ceb82e08b1e34f9ccd6a73bce9339dc435517
Jeremie Kaszub <jeremie.kaszub@gmail.com>
2014-01-22 11:54:10 +0100

    Commit initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Futdechouette::Application.config.secret_key_base = 'b066240b617695392ca2b3bd070fbe4f6d15f5ccba13f707aaa6e66e8a91ddccc30644fba610e10c618026b831f26456177d373471bc5146ef0fb68533fd8cf7'
