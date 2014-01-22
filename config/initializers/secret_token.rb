9010015a77817926a966d77301eb1383035285b9
Jeremie Kaszub <jeremie.kaszub@gmail.com>
2014-01-22 22:12:25 +0100

    Ajout d'un controller

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Futdechouette::Application.config.secret_key_base = 'b066240b617695392ca2b3bd070fbe4f6d15f5ccba13f707aaa6e66e8a91ddccc30644fba610e10c618026b831f26456177d373471bc5146ef0fb68533fd8cf7'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Futdechouette::Application.config.secret_key_base = secure_token
